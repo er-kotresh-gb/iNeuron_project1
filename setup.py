f4408739d1381597a8f7c575b6efea98025f8a51
Kotresh <kotresh.bhuti@emagegroup.com>
2023-12-18 12:51:52 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import find_packages,setup
-from typing import List
+# from typing import List
 
-"""HYPEN_E_DOT='-e .'
+# HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+# def get_requirements(file_path:str)->List[str]:
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+#     return requirements
 
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='KotreshG B',
+    author_email='kotreshbhuti1920@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
